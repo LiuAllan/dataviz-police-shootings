effe94b3e0e9156443e3b56a9a1d44151bf75f59
Allan <allanl_95@hotmail.com>
2020-06-19 21:02:59 -0700

    fix bug

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import MyResponsiveCalendar from './Visualization';
+import MyResponsiveCalendarCanvas from './Visualization';
 
 function App() {
   return (
     <div className="viz-container">
       <h1><b>Fatal</b> Police Shootings in the US (2015-2020)</h1>
-      <MyResponsiveCalendar />
+      <MyResponsiveCalendarCanvas />
       <p>Created by https://github.com/LiuAllan</p>
       <p>Dataset obtained from https://www.kaggle.com/andrewmvd/police-deadly-force-usage-us/data?select=fatal-police-shootings-data.csv</p>
     </div>
